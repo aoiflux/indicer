e52e69749d6febb1c613bc955f38be28add379fd
Gaurav Gogia <16029099+gaurav-gogia@users.noreply.github.com>
2023-05-22 14:04:32 +0530

    restore fix 3

--- lib/store/restore.go
+++ lib/store/restore.go
@@ -36,74 +36,6 @@ func getDBStartOffset(startIndex int64) int64 {
 	return offset
 }
 
-// func findDBStartOffset(startIndex int64, db *badger.DB) (int64, error) {
-// 	if startIndex == 0 {
-// 		return 0, nil
-// 	}
-
-// 	var offset int64
-
-// 	err := db.View(func(txn *badger.Txn) error {
-// 		opts := badger.DefaultIteratorOptions
-// 		opts.PrefetchSize = 1000
-// 		it := txn.NewIterator(opts)
-// 		defer it.Close()
-
-// 		relPrefix := []byte(constant.RelationNapespace)
-// 		for it.Seek(relPrefix); it.ValidForPrefix(relPrefix); it.Next() {
-// 			item := it.Item()
-// 			k := item.KeyCopy(nil)
-
-// 			kpart := bytes.Split(k, relPrefix)[1]
-// 			klist := bytes.Split(kpart, []byte(constant.PipeSeperator))
-// 			dbindexString := string(klist[len(klist)-1])
-// 			dbindex, err := strconv.ParseInt(dbindexString, 10, 64)
-// 			if err != nil {
-// 				return err
-// 			}
-
-// 			// if dbindex > startIndex {
-// 			// 	continue
-// 			// }
-
-// 			v, err := item.ValueCopy(nil)
-// 			if err != nil {
-// 				return err
-// 			}
-// 			decoded, err := s2.Decode(nil, v)
-// 			if err != nil {
-// 				return err
-// 			}
-
-// 			ckey := append([]byte(constant.ChonkNamespace), decoded...)
-// 			item, err = txn.Get(ckey)
-// 			if err != nil {
-// 				return err
-// 			}
-
-// 			v, err = item.ValueCopy(nil)
-// 			if err != nil {
-// 				return err
-// 			}
-// 			decoded, err = s2.Decode(nil, v)
-// 			if err != nil {
-// 				return err
-// 			}
-
-// 			datalen := int64(len(decoded))
-// 			diff := dbindex + datalen
-// 			if (startIndex >= dbindex) && (diff >= startIndex) {
-// 				offset = dbindex
-// 				break
-// 			}
-// 		}
-
-// 		return nil
-// 	})
-
-// 	return offset, err
-// }
-
 func getEvidenceFileHash(fname string) ([]byte, error) {
 	eviFileHashString := strings.Split(fname, constant.FilePathSeperator)[0]
 	eviFileHash, err := base64.StdEncoding.DecodeString(eviFileHashString)
@@ -182,8 +114,7 @@ func restoreData(ehash []byte, start, dbstart, size int64, dst *os.File, db *bad
 			data = data[:size]
 		} else if (restoreIndex + constant.ChonkSize) > end {
 			actualEnd := end - restoreIndex
-			remaining := actualEnd - int64(len(data))
-			data = data[:remaining]
+			data = data[:actualEnd]
 		}
 
 		_, err = dst.Write(data)

5738d2703cd6ecd61b775c9ef960b09c94efc0d8
Gaurav Gogia <16029099+gaurav-gogia@users.noreply.github.com>
2023-09-11 00:08:05 +0530

    bump version

--- main.go
+++ main.go
@@ -29,7 +29,7 @@ func main() {
 	chonkSize := app.Flag(cnst.FlagChonkSize, "Custom chunk size(KB) to be used for dedup").Short(cnst.FlagChonkSizeShort).Default("256").Int()
 	memopt := app.Flag(cnst.FlagLowResource, "Low resource use mode, foregoes performance in favour of utilising less memory, cpu, and energy").Short(cnst.FlagLowResourceShort).Default("false").Bool()
 	QUICKOPT := app.Flag(cnst.FlagFastMode, "Quick mode, forgoes encryption, intra-chunk & overall db compression in favour of higher throughput").Short(cnst.FlagFastModeShort).Default("false").Bool()
-	app.Version("DUES v3.2")
+	app.Version("DUES v3.3")
 
 	cmdstore := app.Command(cnst.CmdStore, "Store file in database")
 	evipath := cmdstore.Arg(cnst.OperandFile, "Path of file that must be saved").Required().String()

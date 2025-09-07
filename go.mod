1fd17d07744cc79dbcfca8841f5f1f9675aead30
Gaurav Gogia <16029099+gaurav-gogia@users.noreply.github.com>
2025-09-07 23:59:32 +0530

    update deps, comment out viz for easy compilation

--- go.mod
+++ go.mod
@@ -1,59 +1,59 @@
 module indicer
 
-go 1.24
+go 1.25
 
 require (
 	github.com/alecthomas/kingpin/v2 v2.4.0
 	github.com/aoiflux/libxfat v1.0.2
 	github.com/cheggaaa/pb/v3 v3.1.7
-	github.com/dgraph-io/badger/v4 v4.6.0
-	github.com/diskfs/go-diskfs v1.5.2
+	github.com/dgraph-io/badger/v4 v4.8.0
+	github.com/diskfs/go-diskfs v1.7.0
 	github.com/dustin/go-humanize v1.0.1
 	github.com/edsrzf/mmap-go v1.2.0
 	github.com/fatih/color v1.18.0
-	github.com/goccy/go-graphviz v0.2.9
 	github.com/klauspost/compress v1.18.0
 	github.com/schollz/progressbar/v3 v3.18.0
 	github.com/shirou/gopsutil/v3 v3.24.5
 	github.com/vmihailenco/msgpack/v5 v5.4.1
-	golang.org/x/crypto v0.36.0
+	golang.org/x/crypto v0.41.0
 )
 
 require (
 	github.com/VividCortex/ewma v1.2.0 // indirect
 	github.com/alecthomas/units v0.0.0-20240927000941-0f3dac36c52b // indirect
+	github.com/anchore/go-lzo v0.1.0 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
-	github.com/dgraph-io/ristretto/v2 v2.1.0 // indirect
-	github.com/disintegration/imaging v1.6.2 // indirect
-	github.com/flopp/go-findfont v0.1.0 // indirect
-	github.com/fogleman/gg v1.3.0 // indirect
-	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/dgraph-io/ristretto/v2 v2.3.0 // indirect
+	github.com/djherbis/times v1.6.0 // indirect
+	github.com/elliotwutingfeng/asciiset v0.0.0-20230602022725-51bbb787efab // indirect
+	github.com/go-logr/logr v1.4.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-ole/go-ole v1.3.0 // indirect
-	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	github.com/google/flatbuffers v25.2.10+incompatible // indirect
-	github.com/lufia/plan9stats v0.0.0-20250317134145-8bc96cf8fc35 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/lufia/plan9stats v0.0.0-20250827001030-24949be3fa54 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.16 // indirect
 	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
+	github.com/pierrec/lz4/v4 v4.1.17 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pkg/xattr v0.4.9 // indirect
 	github.com/power-devops/perfstat v0.0.0-20240221224432-82ca36839d55 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
-	github.com/tetratelabs/wazero v1.9.0 // indirect
+	github.com/sirupsen/logrus v1.9.4-0.20230606125235-dd1b4c2e81af // indirect
 	github.com/tklauser/go-sysconf v0.3.15 // indirect
 	github.com/tklauser/numcpus v0.10.0 // indirect
+	github.com/ulikunitz/xz v0.5.11 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
 	github.com/xhit/go-str2duration/v2 v2.1.0 // indirect
 	github.com/yusufpapurcu/wmi v1.2.4 // indirect
 	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
-	go.opentelemetry.io/otel v1.35.0 // indirect
-	go.opentelemetry.io/otel/metric v1.35.0 // indirect
-	go.opentelemetry.io/otel/trace v1.35.0 // indirect
-	golang.org/x/image v0.25.0 // indirect
-	golang.org/x/net v0.38.0 // indirect
-	golang.org/x/sys v0.31.0 // indirect
-	golang.org/x/term v0.30.0 // indirect
-	golang.org/x/text v0.23.0 // indirect
-	google.golang.org/protobuf v1.36.6 // indirect
+	go.opentelemetry.io/otel v1.38.0 // indirect
+	go.opentelemetry.io/otel/metric v1.38.0 // indirect
+	go.opentelemetry.io/otel/trace v1.38.0 // indirect
+	golang.org/x/net v0.43.0 // indirect
+	golang.org/x/sys v0.36.0 // indirect
+	golang.org/x/term v0.34.0 // indirect
+	google.golang.org/protobuf v1.36.8 // indirect
 )
--- lib/near/visualise.go
+++ lib/near/visualise.go
@@ -2,151 +2,145 @@ package near
 
 import (
 	"bytes"
-	"encoding/base64"
-	"fmt"
 	"indicer/lib/cnst"
 	"indicer/lib/dbio"
-	"indicer/lib/structs"
-	"indicer/lib/util"
 	"strings"
 
 	"github.com/dgraph-io/badger/v4"
-	"github.com/goccy/go-graphviz"
-	"github.com/goccy/go-graphviz/cgraph"
 )
 
-type viz struct {
-	graph *cgraph.Graph
-	db    *badger.DB
-}
+// type viz struct {
+// 	graph *cgraph.Graph
+// 	db    *badger.DB
+// }
 
-func visualise(fid []byte, idmap *structs.ConcMap, db *badger.DB) error {
-	var vg viz
-	vg.db = db
+// func visualise(fid []byte, idmap *structs.ConcMap, db *badger.DB) error {
+// 	var vg viz
+// 	vg.db = db
 
-	g := graphviz.New()
-	defer g.Close()
+// 	g := graphviz.New()
+// 	defer g.Close()
 
-	var err error
-	vg.graph, err = g.Graph()
-	if err != nil {
-		return err
-	}
+// 	var err error
+// 	vg.graph, err = g.Graph()
+// 	if err != nil {
+// 		return err
+// 	}
 
-	title, err := vg.graph.CreateNode("title")
-	if err != nil {
-		return err
-	}
-	title.SetLabel("Artefact Relation Graph - Arbitrary & Unique Artefact Name Aliases are used")
-	title.SetShape(cgraph.BoxShape)
-	title.SetColor("blue")
+// 	title, err := vg.graph.CreateNode("title")
+// 	if err != nil {
+// 		return err
+// 	}
+// 	title.SetLabel("Artefact Relation Graph - Arbitrary & Unique Artefact Name Aliases are used")
+// 	title.SetShape(cgraph.BoxShape)
+// 	title.SetColor("blue")
 
-	err = vg.populateGraph(fid, idmap)
-	if err != nil {
-		return err
-	}
+// 	err = vg.populateGraph(fid, idmap)
+// 	if err != nil {
+// 		return err
+// 	}
 
-	vg.graph.SetRankDir(cgraph.LRRank)
-	return g.RenderFilename(vg.graph, "svg", "./graph.svg")
-}
+// 	vg.graph.SetRankDir(cgraph.LRRank)
+// 	return g.RenderFilename(vg.graph, "svg", "./graph.svg")
+// }
 
-func (vg *viz) populateGraph(fid []byte, idmap *structs.ConcMap) error {
-	fidNode, err := vg.groupNodes(fid, true)
-	if err != nil {
-		return err
-	}
+// func (vg *viz) populateGraph(fid []byte, idmap *structs.ConcMap) error {
+// 	fidNode, err := vg.groupNodes(fid, true)
+// 	if err != nil {
+// 		return err
+// 	}
 
-	var count int
-	for id, confidence := range idmap.GetData() {
-		count++
-		toNode, err := vg.groupNodes([]byte(id), true)
-		if err != nil {
-			return err
-		}
+// 	var count int
+// 	for id, confidence := range idmap.GetData() {
+// 		count++
+// 		toNode, err := vg.groupNodes([]byte(id), true)
+// 		if err != nil {
+// 			return err
+// 		}
 
-		ename := fmt.Sprintf("e_%d", count)
-		edge, err := vg.graph.CreateEdge(ename, fidNode, toNode)
-		if err != nil {
-			return err
-		}
-		edge.SetLabel(fmt.Sprintf("related | confidence: %f%%", confidence))
-		edge.SetLabelFontColor("blue")
-		edge.SetColor("blue")
-		width := confidence / 10
-		if width < 0.5 {
-			width = 0.5
-		}
-		edge.SetPenWidth(width)
-	}
+// 		ename := fmt.Sprintf("e_%d", count)
+// 		edge, err := vg.graph.CreateEdge(ename, fidNode, toNode)
+// 		if err != nil {
+// 			return err
+// 		}
+// 		edge.SetLabel(fmt.Sprintf("related | confidence: %f%%", confidence))
+// 		edge.SetLabelFontColor("blue")
+// 		edge.SetColor("blue")
+// 		width := confidence / 10
+// 		if width < 0.5 {
+// 			width = 0.5
+// 		}
+// 		edge.SetPenWidth(width)
+// 	}
 
-	return nil
-}
+// 	return nil
+// }
 
-func (vg *viz) groupNodes(id []byte, isTarget bool, tonodes ...*cgraph.Node) (*cgraph.Node, error) {
-	names, err := GetNames(id, vg.db, true)
-	if err != nil {
-		return nil, err
-	}
+// func (vg *viz) groupNodes(id []byte, isTarget bool, tonodes ...*cgraph.Node) (*cgraph.Node, error) {
+// 	names, err := GetNames(id, vg.db, true)
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	var idx int
-	var idnode *cgraph.Node
-	for name := range names {
-		idx++
+// 	var idx int
+// 	var idnode *cgraph.Node
+// 	for name := range names {
+// 		idx++
 
-		split := strings.Split(name, cnst.DataSeperator)
-		name = split[len(split)-1]
-		node, err := vg.graph.CreateNode(name)
-		if err != nil {
-			return nil, err
-		}
+// 		split := strings.Split(name, cnst.DataSeperator)
+// 		name = split[len(split)-1]
+// 		node, err := vg.graph.CreateNode(name)
+// 		if err != nil {
+// 			return nil, err
+// 		}
 
-		if len(tonodes) == 0 {
-			split := bytes.Split(id, []byte(cnst.NamespaceSeperator))
-			encoded := base64.StdEncoding.EncodeToString(split[1])
-			idnode, err = vg.graph.CreateNode(encoded)
-			if err != nil {
-				return nil, err
-			}
+// 		if len(tonodes) == 0 {
+// 			split := bytes.Split(id, []byte(cnst.NamespaceSeperator))
+// 			encoded := base64.StdEncoding.EncodeToString(split[1])
+// 			idnode, err = vg.graph.CreateNode(encoded)
+// 			if err != nil {
+// 				return nil, err
+// 			}
 
-			edge, err := vg.graph.CreateEdge(name, idnode, node)
-			if err != nil {
-				return nil, err
-			}
-			edge.SetLabel("alias")
-		}
+// 			edge, err := vg.graph.CreateEdge(name, idnode, node)
+// 			if err != nil {
+// 				return nil, err
+// 			}
+// 			edge.SetLabel("alias")
+// 		}
 
-		for _, tonode := range tonodes {
-			edge, err := vg.graph.CreateEdge(name, node, tonode)
-			if err != nil {
-				return nil, err
-			}
-			edge.SetLabel("child")
-		}
+// 		for _, tonode := range tonodes {
+// 			edge, err := vg.graph.CreateEdge(name, node, tonode)
+// 			if err != nil {
+// 				return nil, err
+// 			}
+// 			edge.SetLabel("child")
+// 		}
 
-		if len(split) > 1 {
-			encoded := split[len(split)-2]
-			decoded, err := base64.StdEncoding.DecodeString(encoded)
-			if err != nil {
-				return nil, err
-			}
+// 		if len(split) > 1 {
+// 			encoded := split[len(split)-2]
+// 			decoded, err := base64.StdEncoding.DecodeString(encoded)
+// 			if err != nil {
+// 				return nil, err
+// 			}
 
-			id := util.AppendToBytesSlice(cnst.PartiFileNamespace, decoded)
-			if len(split) == 2 {
-				id = util.AppendToBytesSlice(cnst.EviFileNamespace, decoded)
-			}
+// 			id := util.AppendToBytesSlice(cnst.PartiFileNamespace, decoded)
+// 			if len(split) == 2 {
+// 				id = util.AppendToBytesSlice(cnst.EviFileNamespace, decoded)
+// 			}
 
-			if isTarget {
-				node = idnode
-			}
-			_, err = vg.groupNodes(id, false, node)
-			if err != nil {
-				return nil, err
-			}
-		}
-	}
+// 			if isTarget {
+// 				node = idnode
+// 			}
+// 			_, err = vg.groupNodes(id, false, node)
+// 			if err != nil {
+// 				return nil, err
+// 			}
+// 		}
+// 	}
 
-	return idnode, nil
-}
+// 	return idnode, nil
+// }
 
 func GetNames(id []byte, db *badger.DB, unique ...bool) (map[string]struct{}, error) {
 	var uniqueFlag bool
